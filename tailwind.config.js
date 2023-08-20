fe9873eb2196bacb76caa5930ac2938f8a065fad
abrhamadddis <abrhamaddis32@gmail.com>
2023-08-20 23:37:20 +0300

    seting up basic env

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,47 +1,55 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
+
+  content: ["./src/**/*.html"],
   theme: {
-    screen:{
-      sm:'375px',
-      lg:'1440px',
+    screen: {
+      sm: '375px',
+      lg: '1440px'
     },
     extend: {
       colors: {
-        limeGreen: 'hsl(163, 72%, 41%)',
-        brightRed: 'hsl(356, 69%, 56%)',
-        faceBook: 'hsl(208, 92%, 53%)',
-        twitter: 'hsl(203, 89%, 53%)',
-        instagramFrom:'hsl(37, 97%, 70%) to ',
-        instagramto: 'hsl(329, 70%, 58%)',
-        youTube: 'hsl(348, 97%, 39%)',
-        toggleFrom: 'hsl(210, 78%, 56%)',
-        toggleTo: 'hsl(146, 68%, 55%)',
-        togglelight: 'hsl(230, 22%, 74%)',
-        //DarkTheme
-        veryDarkBlue:'hsl(230, 17%, 14%)',
-        veryDarkBlue: 'hsl(232, 19%, 15%)',
-        darkDesaturatedBlue: 'hsl(228, 28%, 20%)',
-        desaturatedBlue: 'hsl(228, 34%, 66%)',
-        white: 'hsl(0, 0%, 100%)',
-        //lightTheme
-        whiteBg: 'hsl(0, 0%, 100%)',
-        veryPaleBlue: 'hsl(225, 100%, 98%)',
-        lightGrayishBlue: 'hsl(227, 47%, 96%)',
-        darkGrayishBlueText: 'hsl(228, 12%, 44%)',
-        veryDarkBlueText: 'hsl(230, 17%, 14%)',
+        primary: {
+          limeGreen: 'hsl(163, 72%, 41%)',
+          brightRed: 'hsl(356, 69%, 56%)',
+          facebook: 'hsl(208, 92%, 53%)',
+          twitter: 'hsl(203, 89%, 53%)',
+          instagram: {
+            from: 'hsl(37, 97%, 70%)',
+            to: 'hsl(329, 70%, 58%)'
+          },
+          youtube: 'hsl(348, 97%, 39%)'
+        },
+        darkTheme: {
+          toggle: {
+            from: 'hsl(210, 78%, 56%)',
+            to: 'hsl(146, 68%, 55%)'
+          },
+          veryDarkBlue: 'hsl(230, 17%, 14%)',
+          veryDarkBlueTopBg: 'hsl(232, 19%, 15%)',
+          darkDesaturatedBlue: 'hsl(228, 28%, 20%)',
+          desaturatedBlue: 'hsl(228, 34%, 66%)',
+          white: 'hsl(0, 0%, 100%)'
+        },
+        lightTheme: {
+          toggle: 'hsl(230, 22%, 74%)',
+          white: 'hsl(0, 0%, 100%)',
+          veryPaleBlue: 'hsl(225, 100%, 98%)',
+          lightGreyishBlue: 'hsl(227, 47%, 96%)',
+          darkGreyishBlue: 'hsl(228, 12%, 44%)',
+          veryDarkBlue: 'hsl(230, 17%, 14%)'
+        }
       },
-      fontFamily:{
-        fontFamily1: ['Inter', 'sans-serif'],
-        fontfamily2: ['Poppins', 'sans-serif'],
-        fontfamily3: ['Roboto', 'sans-serif'],
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
       },
       fontWeight: {
-        thin: '400',
-        bold: '700',
-      },  
+        normal: '400',
+        bold: '700'
+      }
     },
   },
   plugins: [],
 }
-
